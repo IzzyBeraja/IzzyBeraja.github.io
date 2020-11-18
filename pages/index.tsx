c9978310c595d38038df65e011a6081be243a337
IzzyBeraja <izzyberaja+github@gmail.com>
2020-11-17 23:50:11 -0500

    TypeScript is now available! 📜

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import styles from "styles/Home.module.css";
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Home | IzzyBeraja.com</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
