d65756024e263cd27cbc9c2615999690658dcb1f
IzzyBeraja <izzyberaja+github@gmail.com>
2020-11-22 01:30:46 -0500

    Separated types into their own file 👐🏻

---
+++ types.ts
@@ -0,0 +1,13 @@
+export type Commit = {
+  id: string;
+  message: string;
+  date: string;
+  commitLink: string;
+};
+
+export type GitHubProfile = {
+  name: string;
+  image: string;
+  link: string;
+  bio: string;
+};
