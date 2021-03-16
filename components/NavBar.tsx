c0615100d343f4cdfe369ff8d51c96a3ac71ad98
IzzyBeraja <izzyberaja+github@gmail.com>
2021-03-16 13:40:06 -0400

    Minor changes to NavBar layout

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,35 +2,42 @@ import styles from "../styles/Navbar.module.css";
 
 const NavBar = () => {
   return (
-    <nav className={styles.navbar}>
-      <ul className={styles.navbar_list}>
-        <li>
-          <a className={styles.navbar_item} href="#Home">
-            Home
-          </a>
-        </li>
-        <li>
-          <a className={styles.navbar_item} href="#Skills">
-            Skills
-          </a>
-        </li>
-        <li>
-          <a className={styles.navbar_item} href="#Work">
-            Work
-          </a>
-        </li>
-        <li>
-          <a className={styles.navbar_item} href="#OpenSource">
-            Open Source
-          </a>
-        </li>
-        <li>
-          <a className={styles.navbar_item} href="#">
-            Resume
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div className={styles.header}>
+      <span>
+        <i
+          className={["fas fa-hamburger", styles.navbar_hamburger].join(" ")}
+        ></i>
+      </span>
+      <nav className={styles.navbar}>
+        <ul className={styles.navbar_list}>
+          <li>
+            <a className={styles.navbar_item} href="#Home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className={styles.navbar_item} href="#Skills">
+              Skills
+            </a>
+          </li>
+          <li>
+            <a className={styles.navbar_item} href="#Work">
+              Work
+            </a>
+          </li>
+          <li>
+            <a className={styles.navbar_item} href="#OpenSource">
+              Open Source
+            </a>
+          </li>
+          <li>
+            <a className={styles.navbar_item} href="#">
+              Resume
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
