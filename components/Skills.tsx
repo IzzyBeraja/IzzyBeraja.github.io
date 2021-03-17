60695c77d4bb2c646113b0b9f1dcd0582230db0e
IzzyBeraja <izzyberaja+github@gmail.com>
2021-03-17 16:37:25 -0400

    Updated Skills to have pics, and new styles. Default values now exist in separate file.

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,51 +1,25 @@
 import Card from "../components/Card";
 
+import { skillInfo } from "../content/SkillsInfo";
+
 import styles from "../styles/Skills.module.css";
 
-type SkillInfo = {
+export type SkillInfo = {
   title: string;
   description: string;
+  color?: string;
+  logo?: string;
 };
 
 const Skills = () => {
-  const skillInfo: SkillInfo[] = [
-    {
-      title: "C#",
-      description:
-        "My first programming language. I learned it in highschool and perfected it over the next few years.",
-    },
-    {
-      title: "JS",
-      description: "Front end programming that I use on a daily basis",
-    },
-    {
-      title: "TS",
-      description: "The JavaScript we deserve",
-    },
-    {
-      title: "SQL",
-      description: "The JavaScript we deserve",
-    },
-    { title: "Docker", description: "The JavaScript we deserve" },
-    {
-      title: "Mongo",
-      description: "The JavaScript we deserve",
-    },
-    {
-      title: "React",
-      description: "The JavaScript we deserve",
-    },
-    {
-      title: "NextJS",
-      description: "The JavaScript we deserve",
-    },
-  ];
-
   return (
     <div id="Skills" className="container">
+      <h1 className={styles.title}>
+        My <span>Skillset</span>
+      </h1>
       <div className={styles.grid}>
-        {skillInfo.map(({ title, description }, i) => (
-          <Card key={i} title={title} description={description} />
+        {skillInfo.map(({ title, description, logo }, i) => (
+          <Card key={i} title={title} description={description} logo={logo} />
         ))}
       </div>
     </div>
---
+++ content/SkillsInfo.ts
@@ -0,0 +1,50 @@
+import { SkillInfo } from "../components/Skills";
+
+export const skillInfo: SkillInfo[] = [
+  {
+    title: "C#",
+    description:
+      "I think everyone has a sweetspot for their first programming language. C# is not mine; however I love it just the same.",
+    logo: "CSharp-logo.svg",
+  },
+  {
+    title: "JS",
+    description:
+      "I use it on a daily basis, but it's like programming in a minefield. Who needs type safety am I right?",
+    logo: "JavaScript-logo.svg",
+  },
+  {
+    title: "TS",
+    description:
+      "Coming from an Object Oriented household, I can't possibly imagine writing JavaScript without TypeScript.",
+    logo: "TypeScript-logo.svg",
+  },
+  {
+    title: "SQL",
+    description: "If only real life relationships were as easy as SQL joins.",
+    logo: "SQL-logo.svg",
+  },
+  {
+    title: "Docker",
+    description:
+      "It's like moving without the heavy lifting and breaking on delivery. Put it in a container and ship it!",
+    logo: "Docker-logo.png",
+  },
+  {
+    title: "Mongo",
+    description: "No schema no cry!",
+    logo: "MongoDB-logo.png",
+  },
+  {
+    title: "React",
+    description:
+      "The only frontend framework that has really kept me hooked! Get it? Hooked! Cause of all the hooks...",
+    logo: "React-logo.png",
+  },
+  {
+    title: "GraphQL",
+    description:
+      "REST's more capable cousin with a touch of being much more complicated.",
+    logo: "GraphQL-Logo.svg",
+  },
+];
