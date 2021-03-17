2637130294856dd5d496e4db920225d0a9c2311a
IzzyBeraja <izzyberaja+github@gmail.com>
2021-03-17 16:35:54 -0400

    Changed home structure and some styles 🏡

--- components/Home.tsx
+++ components/Home.tsx
@@ -3,11 +3,15 @@ import styles from "../styles/Home.module.css";
 const Home = () => {
   return (
     <main className={styles.main}>
-      <h2>Maker</h2>
-      <h2>Creator</h2>
-      <h2>Engineer</h2>
-      <h2>Developer</h2>
-      <h1>Izzy Beraja</h1>
+      <div className={styles.desc}>
+        <h2>Maker</h2>
+        <h2>Creator</h2>
+        <h2>Engineer</h2>
+        <h2>Developer</h2>
+      </div>
+      <div className={styles.title}>
+        <h1>Izzy Beraja</h1>
+      </div>
     </main>
   );
 };
