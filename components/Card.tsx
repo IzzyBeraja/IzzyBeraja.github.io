8b937ae1304052fe28cf543dd3556aeeefd486e5
IzzyBeraja <izzyberaja+github@gmail.com>
2021-03-17 16:35:16 -0400

    Added variables and some Card functionality

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,14 +4,20 @@ interface Props {
   className?: string;
   title: string;
   description: string;
+  logo?: string;
 }
 
-const Card = ({ className, title, description }: Props) => {
+const cardBackground = (logo: string, title: string) => {
+  if (!logo) return <h3 className={styles.background_text}>{title}</h3>;
+  return <img className={styles.background_logo} src={logo} alt={title} />;
+};
+
+const Card = ({ className, title, description, logo }: Props) => {
   return (
     <div className={[className, styles.card].join(" ")}>
-      <h3 className={[styles.foreground_text].join(" ")}>{title}</h3>
-      <h3 className={[styles.background_text].join(" ")}>{title}</h3>
-      <p className={[styles.description].join(" ")}>{description}</p>
+      <h3 className={styles.foreground_text}>{title}</h3>
+      {cardBackground(logo, title)}
+      <p className={styles.description}>{description}</p>
     </div>
   );
 };
